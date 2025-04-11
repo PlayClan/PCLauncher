d81eaad0f5b2865215b05a68100d6ccfceced256
Medve Adrián <112695575+ADRIAN3003@users.noreply.github.com>
2025-04-11 23:33:12 +0200

    Mask access token in logs.

--- app/assets/js/processbuilder.js
+++ app/assets/js/processbuilder.js
@@ -72,7 +72,10 @@ class ProcessBuilder {
             args = args.concat(this.constructModList(modObj.fMods))
         }
 
-        logger.info('Launch Arguments:', args)
+        const loggableArgs = [...args]
+        loggableArgs[loggableArgs.findIndex(x => x === this.authUser.accessToken)] = '**********'
+
+        logger.info('Launch Arguments:', loggableArgs)
 
         const child = child_process.spawn(ConfigManager.getJavaExecutable(this.server.rawServer.id), args, {
             cwd: this.gameDir,
